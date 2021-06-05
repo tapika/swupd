9625749ee27650c27d710aceb8516d8951ab41dc
Tarmo Pikaro <tapika@yahoo.com>
2021-06-05 15:44:22 +0300

    hardcode version, take 2

--- build.ps1
+++ build.ps1
@@ -315,7 +315,7 @@ foreach ($operation in $operationsToPerform)
             exit 2
         }
         
-        $versionNumber = 1.0.0
+        $versionNumber = '1.0.0'
         #$versionNumber = git describe --abbrev=0
         $commitId = $env:APPVEYOR_REPO_COMMIT
 
