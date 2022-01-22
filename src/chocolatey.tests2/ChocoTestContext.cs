2a1259dbcba5a7dde5a47deb95e2c86d5867c75b
Tarmo Pikaro <tapika@yahoo.com>
2022-01-22 12:56:09 +0200

    commit missing file

---
+++ src/chocolatey.tests2/ChocoTestContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace chocolatey.tests2
+{
+    public enum ChocoTestContext
+    {
+        installupdate,
+        exactpackage
+    };
+}
