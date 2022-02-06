12faa4ea8dcf56ae2db25a2747f722b6a4941bad
Tarmo Pikaro <tapika@yahoo.com>
2022-02-06 07:45:22 +0200

    tune: Disable printing of $packageFolder - no need for absolute paths in unit testing

--- src/chocolatey.tests.integration/context/installpackage/1.0.0/tools/chocolateyinstall.ps1
+++ src/chocolatey.tests.integration/context/installpackage/1.0.0/tools/chocolateyinstall.ps1
@@ -1,10 +1,10 @@
 ﻿$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 "simple file" | Out-File "$toolsDir\simplefile.txt" -force
 
-Write-Output "This is $packageName v$packageVersion being installed to `n $packageFolder"
+Write-Output "This is $packageName v$packageVersion being installed"
 Write-Host "Ya!"
 Write-Debug "A debug message"
 Write-Verbose "Yo!"
 Write-Warning "A warning!"
 
-Write-Output "$packageName v$packageVersion has been installed to `n $packageFolder"
+Write-Output "$packageName v$packageVersion has been installed"
