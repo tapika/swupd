419d5e981d53cc3de732d912e7df580a2d27472e
Tarmo Pikaro <tapika@yahoo.com>
2022-03-12 07:05:24 +0200

    tune: commit missing files

---
+++ src/chocolatey/infrastructure.app/commands/ChocolateyCommand.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using chocolatey.infrastructure.commandline;
+using chocolatey.infrastructure.app.configuration;
+using chocolatey.infrastructure.commands;
+
+namespace chocolatey.infrastructure.app.commands
+{
+    /// <summary>
+    /// Dummy empty command implementation.
+    /// </summary>
+    public class ChocolateyCommand : ICommand
+    {
+        public virtual void configure_argument_parser(OptionSet optionSet, ChocolateyConfiguration config)
+        {
+        }
+
+        public virtual void handle_additional_argument_parsing(IList<string> unparsedArguments, ChocolateyConfiguration configuration)
+        {
+        }
+
+        public virtual void handle_validation(ChocolateyConfiguration configuration)
+        {
+        }
+
+        public virtual void help_message(ChocolateyConfiguration configuration)
+        {
+        }
+
+        public virtual void noop(ChocolateyConfiguration configuration)
+        {
+        }
+
+        public virtual void run(ChocolateyConfiguration configuration)
+        {
+        }
+
+        public virtual bool may_require_admin_access()
+        {
+            return false;
+        }
+    }
+}
+
---
+++ src/chocolatey/infrastructure.app/commands/ChocolateyStartupCommand.cs
@@ -0,0 +1,32 @@
+﻿namespace chocolatey.infrastructure.app.commands
+{
+    using System.Collections.Generic;
+    using commandline;
+    using configuration;
+    using infrastructure.commands;
+
+    /// <summary>
+    /// Artificial command used to initialize application startup parameters.
+    /// </summary>
+    public class ChocolateyStartupCommand : ChocolateyCommand
+    {
+        override public void configure_argument_parser(OptionSet optionSet, ChocolateyConfiguration config)
+        {
+            configureStartupOptions(optionSet);
+        }
+
+        public static void configureStartupOptions(OptionSet optionSet)
+        {
+            // For some reason not empty when called for second time via Lets.GetChocolatey()
+            if (!optionSet.Contains("root"))
+            {
+                optionSet
+                    .Add("root=", "Sets root location",
+                        option => InstallContext.Instance.RootLocation = option.remove_surrounding_quotes())
+                    .Add("clearlogs|clearlog", "Removes logs if any is present",
+                        option => ApplicationParameters.ClearLogFiles = option != null)
+                ;
+            }
+        }
+    }
+}
