a0e668a32d0455ad969c6a6bceaf6bf6204bd800
Tarmo Pikaro <tapika@yahoo.com>
2022-03-30 20:57:46 +0300

    tune: Add desktop application with shortcut creation

---
+++ lib/msgbox/msgbox.cpp
@@ -0,0 +1,8 @@
+#include <windows.h>
+
+int WINAPI WinMain (HINSTANCE hInstance, HINSTANCE hPrevInstance,PSTR szCmdLine, int iCmdShow)
+{
+    MessageBox (NULL, "Hello world", "message", MB_OK);
+    return 0 ;
+}
+
--- src/chocolatey.tests.integration/context/installpackage2/1.0.0/tools/chocolateyinstall.ps1
+++ src/chocolatey.tests.integration/context/installpackage2/1.0.0/tools/chocolateyinstall.ps1
@@ -1,1 +1,18 @@
-﻿Write-Output "$packageName with custom dir was installed"
+﻿$wshshell = New-Object -ComObject WScript.Shell
+
+$startMenu = $([System.Environment]::GetFolderPath([System.Environment+SpecialFolder]::CommonStartMenu))
+$shortcutPath = "$startMenu\Programs\msgbox.lnk"
+
+$exePath = "$packageFolder\msgbox.exe"
+
+if (!(Test-Path($exePath)))
+{
+  # powershell script executed locally
+  $scriptDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+  $exePath = "$scriptDir\..\msgbox.exe"
+}
+
+$lnk = $wshshell.CreateShortcut($shortcutPath)
+$lnk.TargetPath = $exePath
+$lnk.Save()
+
