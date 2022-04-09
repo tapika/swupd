6f8738a26f09d78fe41ab4a3358ef2ccedb7d1c1
Tarmo Pikaro <tapika@yahoo.com>
2022-04-09 12:20:22 +0300

    17/36 when_upgrading_a_package_with_added_files test upgrade from UpgradeScenarios.cs

--- src/chocolatey.tests2/commands/TestUpgradeCommand.cs
+++ src/chocolatey.tests2/commands/TestUpgradeCommand.cs
@@ -303,6 +303,18 @@ namespace chocolatey.tests2.commands
             }
         }
 
+        [LogTest]
+        public void when_upgrading_a_package_with_added_files()
+        {
+            string path = null;
+            TestUpgrade((conf) =>
+            {
+                path = Path.Combine(InstallContext.Instance.PackagesLocation, conf.PackageNames, "dude.txt");
+                File.WriteAllText(path, "hellow");
+            });
+            Assert.True(File.Exists(path));
+        }
+
     }
 }
 
