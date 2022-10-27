9690e9f0fc2d6c336842d320524b108d9766f3fd
Tarmo Pikaro <tapika@yahoo.com>
2022-10-27 18:00:02 +0300

    powershell backwards compatibility #6

---
+++ src/chocolatey.resources/helpers/functions/PowerShellNonCoreCompatibility.ps1
@@ -0,0 +1,30 @@
+# Function can be tested in pwsh using following variable defined:
+#$PSModuleAutoloadingPreference = 'None'
+
+if ($null -eq (Get-Command -Name Get-WmiObject -ErrorAction SilentlyContinue))
+{
+    Import-Module CimCmdlets
+
+    # powershell core's wrapper function can be found using commands:
+    #
+    # Get-Command Get-WmiObject
+    # ${function:Get-WmiObject}.File
+
+    # This is a wrapper function from Get-WmiObject to Get-CimInstance
+    # not all parameters were verified but commonly used ones
+    function Get-WmiObject(
+        [Alias('ClassName')]
+        ${Class},
+
+        [string]$Filter = $null
+    )
+    {
+        if($PSBoundParameters.ContainsKey('Filter'))
+        {
+            return Get-CimInstance -ClassName $Class -Filter $Filter
+        }
+        
+        return Get-CimInstance -ClassName $Class
+    }
+}
+
