069f40ff64a36adbf03fc7103293384b2b61da55
Tarmo Pikaro <tapika@yahoo.com>
2022-11-13 00:21:49 +0200

    tune: build shimgen before main solution

--- cakebuild/commands/buildsolution.cs
+++ cakebuild/commands/buildsolution.cs
@@ -36,32 +36,41 @@ namespace cakebuild.commands
             }
 
             string rootDir = context.RootDirectory;
-            string solutionPath = Path.Combine(rootDir, $@"src\chocolatey{context.cmdArgs.NetFrameworkSuffix}.sln");
 
-            if (context.cmdArgs.DryRun)
+            string[] solutions = new[] { 
+                // Build tool first so it can be used for chocolatey
+                @"src\shimgen\shimgen.sln", 
+                $@"src\chocolatey{context.cmdArgs.NetFrameworkSuffix}.sln" };
+
+            foreach (string solution in solutions)
             {
-                LogInfo($"> would build {solutionPath}");
-                return;
-            }
+                string solutionPath = Path.Combine(rootDir, solution);
 
-            LogInfo($"- Building {Path.GetFileName(solutionPath)}...");
-
-
-            context.ConfigureDotNet(Path.Combine(rootDir, "src"));
-
-            DotNetCoreMSBuildSettings msbuild_settings = new DotNetCoreMSBuildSettings()
-            {
-                Verbosity = DotNetVerbosity.Minimal
-            };
-
-            context.DotNetBuild(solutionPath,
-                new DotNetBuildSettings
+                if (context.cmdArgs.DryRun)
                 {
-                    Configuration = "Release", 
-                    NoLogo = true,
-                    MSBuildSettings = msbuild_settings
+                    LogInfo($"> would build {solutionPath}");
+                    return;
                 }
-            );
+
+                LogInfo($"- Building {Path.GetFileName(solutionPath)}...");
+
+
+                context.ConfigureDotNet(Path.Combine(rootDir, "src"));
+
+                DotNetCoreMSBuildSettings msbuild_settings = new DotNetCoreMSBuildSettings()
+                {
+                    Verbosity = DotNetVerbosity.Minimal
+                };
+
+                context.DotNetBuild(solutionPath,
+                    new DotNetBuildSettings
+                    {
+                        Configuration = "Release",
+                        NoLogo = true,
+                        MSBuildSettings = msbuild_settings
+                    }
+                );
+            }
         }
     }
 
